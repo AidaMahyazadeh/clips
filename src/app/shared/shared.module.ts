cba6f57354381b0ea200ca5c3d06a7306ca9baf9
AidaMahyazadeh <aidamahyazadeh83@gmail.com>
2023-01-11 08:24:26 +0100

    fixed register form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabContainerComponent } from './tab-container/tab-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AlertComponent } from './alert/alert.component';
+
 
 
 
@@ -10,15 +15,23 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+   
+
   ],
   exports:[
     ModalComponent,
     TabComponent,
-    TabContainerComponent
+    TabContainerComponent,
+    InputComponent,
+    AlertComponent
+  
   ]
 })
 export class SharedModule { }
