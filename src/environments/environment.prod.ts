3de73a988977bd812beabdbb6ba1663984f90d8c
AidaMahyazadeh <aidamahyazadeh83@gmail.com>
2023-01-16 12:13:10 +0100

    insertet firebase object as property

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment={
+    production:true,
+    firebase:{
+        apiKey: "AIzaSyCbzwvfeYdEP2RODB8BMdMHz4-JFLo1PEE",
+        authDomain: "clips-12237.firebaseapp.com",
+        projectId: "clips-12237",
+        storageBucket: "clips-12237.appspot.com",
+        appId: "1:466765561258:web:fe1e4fc858b17c1ea2bfd3"
+      }
+}
