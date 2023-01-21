d6cd2fe3824991a75bbc1d4ce4dac76b1ad46e6f
AidaMahyazadeh <aidamahyazadeh83@gmail.com>
2023-01-21 11:17:50 +0100

    created difference component for routes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,28 +2,41 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
-import { Observable , delay, map } from 'rxjs';
+import { Observable, delay, map, filter, switchMap, of } from 'rxjs';
 import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private userCollection: AngularFirestoreCollection<IUser>
-  public isAuthenticated$ :Observable <boolean>
-  public isAuthenticatedWithDelay$ :Observable <boolean>
-  
+  public isAuthenticated$: Observable<boolean>
+  public isAuthenticatedWithDelay$: Observable<boolean>
+  private redirect = false
+
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private rout: ActivatedRoute,
+
   ) {
     this.userCollection = db.collection('users')
-    this.isAuthenticated$=auth.user.pipe(
-      map(user=>!!user)
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
     )
-    this.isAuthenticatedWithDelay$=this.isAuthenticated$.pipe(
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
       delay(1000)
     )
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.rout.firstChild),
+      switchMap(rout => rout?.data ?? of({}))
+    ).subscribe(data => {
+      this.redirect = data.authOnly??false
+    })
   }
 
   public async createUser(userData: IUser) {
@@ -42,11 +55,14 @@ export class AuthService {
     })
   }
 
- public async logout($event?: Event) {
-  if($event){
-    $event.preventDefault()
-  }
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault()
+    }
     await this.auth.signOut()
-    await this.router.navigateByUrl('/')
+
+   if (this.redirect){
+      await this.router.navigateByUrl("/")
+   }
   }
 }
